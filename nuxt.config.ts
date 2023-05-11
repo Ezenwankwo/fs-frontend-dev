89c9f5c02bab085251303a1bb8c9442d7d7682e7
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2023-05-11 19:02:50 +0100

    fix deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
-           baseURL: 'http://localhost:8000' //'https://finstack.herokuapp.com'
+           baseURL: 'https://finstack.herokuapp.com' //'http://localhost:8000' 
         }
     },
 })
