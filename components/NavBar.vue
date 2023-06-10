a3f98a423f0d62beb76d8636a38da9e7063eb70c
Peter Olayinka <peterolayinka97@gmail.com>
2023-06-10 18:53:10 +0100

    Add nav bar and nav toggle on mobile.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,86 +1,45 @@
 <template>
   <nav class="nav-fixed">
     <div class="container">
-      <div class="sidebar" id="sidebar">
-        <div class="sd">
-          <div class="main_sidenav">
-            <header>
-              <nav>
-                <a href="" class="logo_a">
-                  <img src="~/assets/logo.png " alt="wame" class="logo" /></a
-                ><i class="x ii material-icons" id="x">close</i>
-              </nav>
-            </header>
-            <div class="nav_items">
-              <span class="sp">
-                <a class="a" href="javascript:void(0)"
-                  >Home <i class="material-icons">navigate_next</i></a
-                > </span
-              ><span class="sp">
-                <a class="a" href="javascript:void(0)"
-                  >Our blog <i class="material-icons">navigate_next</i></a
-                ></span
-              ><span class="sp">
-                <a class="a" href="javascript:void(0)"
-                  >Become a seller
-                  <i class="material-icons">navigate_next</i></a
-                ></span
-              ><span class="sp">
-                <a class="a" href="javascript:void(0)"
-                  >P2P marketplace<i class="material-icons">navigate_next</i></a
-                ></span
-              ><span class="sp">
-                <a class="a" href="javascript:void(0)">Contact us</a></span
-              ><span class="sp">
-                <a class="a" href="javascript:void(0)"
-                  >All currency listings</a
-                ></span
-              >
-              <div class="st">
-                <span class="sp1"
-                  ><a href=""><i class="fa-brands fa-facebook"></i></a
-                  ><a href=""
-                    ><i class="fa fa-twitter" aria-hidden="true"></i></a
-                  ><a href=""
-                    ><i
-                      class="fa fa-instagram"
-                      aria-hidden="true"
-                    ></i></a></span
-                ><span class="sp2"
-                  ><a href="">Terms and conditions. Privacy Policy</a
-                  ><a href="">© All rights reserved. Finstack 2022.</a></span
-                >
-              </div>
-            </div>
-            <div class="in">
-              <NuxtLink class="a" to="/login">
-                Sign in
-                <i class="material-icons">
-                  <Icon name="ic:round-chevron-right" />
-                </i>
-              </NuxtLink>
-            </div>
-          </div>
-          <div class="products"></div>
+      <div class="main-nav flex w-full">
+        <a class="logo_a" href="/">
+          <img src="~/assets/logo.png " alt="wame" class="logo" />
+        </a>
+        <!-- <div class="sp">
+          <a class="link" href="/blog_index">Our blog</a
+          ><a class="link" href="/contactus">Contact us</a
+          ><a class="link" href="/seller_up">Become a seller</a
+          ><a class="link" href="/P2PIndex">P2P marketplace</a>
+        </div> -->
+        <div class="log">
+          <NuxtLink class="link login" to="/login">Login</NuxtLink>
+          <NuxtLink class="link signup" to="/signup">Get Started</NuxtLink>
         </div>
+        <span class="mobile-menu" @click="toggleMenu">
+          <Icon name="ic:baseline-menu" />
+        </span>
       </div>
-      <a class="logo_a" href="/">
-        <img src="~/assets/logo.png " alt="wame" class="logo" />
-      </a>
-      <!-- <div class="sp">
-              <a class="link" href="/blog_index">Our blog</a><a class="link" href="/contactus">Contact us</a><a class="link"
-                href="/seller_up">Become a seller</a><a class="link" href="/P2PIndex">P2P marketplace</a>
-            </div> -->
-      <div class="log">
-        <NuxtLink class="link login" to="/login">Login</NuxtLink>
-        <NuxtLink class="link signup" to="/signup">Get Started</NuxtLink>
+      <div v-if="isMenuOpen" class="mobile-menu-links">
+        <ul>
+          <li>
+            <NuxtLink class="link login" to="/login">Login</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="link signup" to="/signup">Get Started</NuxtLink>
+          </li>
+        </ul>
       </div>
-      <i class="i ii material-icons" id="i">
-        <Icon name="ic:baseline-menu" />
-      </i>
     </div>
   </nav>
 </template>
+
+<script setup>
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
 
 <style lang="less" scoped>
 nav {
@@ -93,8 +52,11 @@ nav {
   .container {
     display: flex;
     margin: auto;
-    align-items: center;
     padding: 20px;
+    flex-direction: column;
+  }
+  .main-nav {
+    align-items: center;
   }
 }
 
@@ -274,9 +236,33 @@ nav .user .dwn {
 }
 
 .log {
-    .link {
-        text-decoration: none;
-    }
+  .link {
+    text-decoration: none;
+  }
+}
+
+.mobile-menu {
+  display: none;
+}
+
+.mobile-menu-links {
+  background: #fff;
+  text-align: center;
+  width: 100%;
+  padding: 40px 20px 20px;
+  li {
+    padding: 10px 20px;
+  }
+  a {
+    text-decoration: none;
+  }
+  .signup {
+    padding: 9px 24px;
+    border-radius: 30px;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+  }
 }
 
 @media screen and (max-width: 1100px) {
@@ -300,13 +286,21 @@ nav .user .dwn {
     display: none;
   }
 
-  .i {
+  .mobile-menu {
     color: white;
     background-color: #2f67fa;
     border-radius: 100%;
-    padding: 10px;
     font-size: 16px !important;
-    display: flex !important;
+    width: 35px;
+    height: 35px;
+    text-align: center;
+    display: flex;
+    margin-left: auto;
+    cursor: pointer;
+    svg {
+      align-self: center;
+      width: 100%;
+    }
   }
 
   nav .user .n_icon {
@@ -326,247 +320,11 @@ nav .user .dwn {
   }
 }
 
-.sidebar {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  position: fixed;
-  z-index: 5;
-  width: 100%;
-  height: 100vh;
-  /* background-color: rgba(129, 129, 129, 0.6); */
-  top: -110%;
-  /* top: 0; */
-  left: 0;
-  transition: 300ms;
-  /* background-color: black; */
-  /* border: solid brown; */
-  /* margin: 0 auto; */
-  padding: 0 7%;
-}
-
-.sidebar header {
-  width: 100%;
-}
-
-.sidebar nav {
-  /* border: solid 5px blue; */
-  width: 100% !important;
-  margin: 0 auto;
-  position: static !important;
-  /* pos */
-  height: 70px !important;
-  -webkit-backdrop-filter: 0;
-  backdrop-filter: 0;
-  box-shadow: none;
-  box-shadow: 5px -1px 10px -5px rgb(168, 168, 168);
-}
-
-.sidebar .sd {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  width: 100%;
-  height: 100%;
-  /* margin-top: 100px; */
-  padding: 0;
-  /* background-color: rgb(247, 247, 247); */
-  background-color: white;
-  margin: 15px 0;
-  border-radius: 10px;
-  overflow: hidden;
-  border: solid 5px white;
-  box-shadow: 0 0 3px 1px rgb(128, 128, 128, 0.1);
-  box-shadow: 0px 0px 30px 10px rgba(130, 130, 130, 0.3);
-
-  position: relative;
-}
-
-#x {
-  display: block !important;
-  cursor: pointer;
-}
-
-.sidebar .products {
-  width: 100%;
-  height: 100%;
-  /* position: absolute; */
-  background-color: blue;
-  position: relative;
-  z-index: 2;
-  right: -100%;
-}
-
-.sidebar .sd .main_sidenav {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  overflow: hidden;
-  position: absolute;
-  z-index: 1;
-}
-
-.sidebar .main_sidenav .nav_items {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-direction: column;
-  margin-top: auto;
-  padding: 10px 0;
-  /* border: solid; */
-  overflow-x: scroll;
-}
-
-.sidebar .main_sidenav .nav_items .sp {
-  width: 100%;
-  display: inline-block;
-}
-
-.sidebar .main_sidenav .nav_items .sp .a {
-  width: 100%;
-  padding: 20px 7%;
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: #3f4b66;
-  font-size: 15px;
-  flex-direction: row;
-}
-
-.sidebar .main_sidenav .nav_items .sp + .sp::before {
-  content: "";
-  border-bottom: 1.5px solid rgba(206, 206, 206, 0.2);
-  /* border-spacing: 10px; */
-  border-bottom-style: dashed;
-  /* border-inline-style:; */
-  /* border-style:ridge; */
-  display: block;
-  margin: 0 auto;
-}
-
-.sidebar .main_sidenav .nav_items .st {
-  width: 100%;
-  margin-top: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-end;
-  /* border: solid; */
-  padding: 0 7%;
-}
-
-.sidebar .main_sidenav .nav_items .st .sp1 {
-  width: 100%;
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  flex-direction: row;
-  gap: 10px;
-}
-
-.sidebar .main_sidenav .nav_items .st .sp1 a i {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 32px;
-  width: 32px;
-  font-size: 16px;
-  box-shadow: 0 0 8px 1px rgb(47, 103, 250, 0.2);
-  border-radius: 100%;
-  background-color: #2f67fa;
-  color: white;
-}
-
-.sidebar .main_sidenav .nav_items .st .sp2 {
-  width: 100%;
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  margin-top: 15px;
-  flex-wrap: wrap;
-  margin-bottom: 5px;
-  gap: 10px;
-}
-
-.sidebar .main_sidenav .nav_items .st .sp2 a {
-  display: flex;
-  justify-content: left;
-  align-items: center;
-  flex-direction: row;
-  gap: 5px;
-  color: #3f4b66;
-  font-size: 14px;
-}
-
-.sidebar .main_sidenav .in {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-  margin-top: auto;
-  padding: 10px 0;
-  background-color: rgba(245, 245, 245, 0.7);
-}
-
-.sidebar .main_sidenav .in .a {
-  padding: 8px 20px;
-  border-radius: 30px;
-  background-color: #2f67fa;
-  color: white;
-  font-size: 14px;
-  font-weight: 600;
-  box-shadow: 0 0 8px 3px rgb(47, 103, 250, 0.2);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-  gap: 5px;
-  /* border: solid; */
-}
-
-.sidebar .main_sidenav .in .a i {
-  color: white;
-  font-size: 18px;
-  /* background-color: rgb(247, 247, 247); */
-}
-
 @media screen and (max-width: 1000px) {
-  .sidebar .main_sidenav .nav_items .sp .a {
-    padding: 20px 5%;
-  }
-}
-
-@media screen and (max-width: 1000px) {
-  /* nav{
-        padding: %;   
-    } */
-
   .sp {
     display: none;
   }
-
-  .i {
-    color: #2f67fa;
-    display: flex !important;
-  }
 }
-
-@media screen and (max-width: 600px) {
-  .sidebar .main_sidenav .nav_items .sp .a {
-    padding: 20px 7%;
-  }
-}
-
 .logo {
   width: 55px;
   margin: 0;
@@ -617,15 +375,6 @@ nav .user .dwn {
 
 .signup:hover {
   background-color: rgb(37, 150, 190, 0.8);
-}
-
-/* .material-icons{
-    display: none;
-} */
-
-.i {
-  display: none !important;
-  /* border: solid; */
 }
 
 .x {
