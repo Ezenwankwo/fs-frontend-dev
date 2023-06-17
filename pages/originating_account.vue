5e74028310fc665c372f35d94e09140ef258accb
Peter Olayinka <peterolayinka97@gmail.com>
2023-06-18 00:18:13 +0100

    Create account on the fly.

--- pages/originating_account.vue
+++ pages/originating_account.vue
@@ -21,12 +21,34 @@ definePageMeta({
 
 const store = useConversionStore();
 const { originatingAccount } = storeToRefs(store);
+const exchange = useConversionStore().$state.exchange;
 
-const gotoNextPage = () => {
-  const account = { ...originatingAccount.value };
-  delete account.other_bank;
-  if (!Object.values(account).includes("")) {
+const gotoNextPage = async () => {
+  const {
+    account_type,
+    other_bank,
+    holder_name,
+    number_or_address,
+    bank_or_network,
+  } = originatingAccount.value;
+  const bank = bank_or_network == "Other" ? other_bank : bank_or_network;
+  const body = {
+    account_type: account_type.charAt(0).toUpperCase() + account_type.slice(1),
+    currency: exchange.to_currency,
+    holder_name,
+    bank_or_network: bank,
+    number_or_address,
+  };
+
+  try {
+    const response = await $api(`/wallets/accounts/`, {
+      method: "POST",
+      body,
+    });
+    useConversionStore().setOriginatingAccount(response.data);
     navigateTo("/receiving_account");
+  } catch (error) {
+    useNotification().toast.error(error.data.message);
   }
 };
 </script>
