ba6d6e150967524de3c382caed36188a8acf9c3f
Peter Olayinka <peterolayinka97@gmail.com>
2023-06-18 19:49:47 +0100

    Retorch rating page.

--- pages/rating.vue
+++ pages/rating.vue
@@ -1,40 +1,31 @@
 <template>
   <div>
-    <NavBar>
-    <div class="flex mt-12">
-      <div class="p-12 mx-auto">
+    <NavBar />
+    <div class="flex pt-12">
+      <div class="py-12 px-2 mx-auto w-full rating-wrapper">
         <div class="w-full text-center">
           <img src="~/assets/star.svg" alt="" class="mx-auto" />
           <div class="mb-6">
-            <p class="text-[#373D4A] font-bold">Woohoo!</p>
-            <p class="text-lg text-[#373D4A]">
-              Thank you for choosing FinStack!
-            </p>
+            <p class="greeting--text">Woohoo!</p>
+            <p class="thank-you--text">Thank you for choosing FinStack!</p>
           </div>
-          <div class="mb-6">
-            <p class="text-[#373D4A] mb-2">Rate our service</p>
+          <div class="rating--box">
+            <p class="rating--text">Rate our service</p>
             <div class="rating">
-              <input type="radio" name="rating-1" class="mask mask-star" />
-              <input
-                type="radio"
-                name="rating-1"
-                class="mask mask-star"
-                checked
-              />
-              <input type="radio" name="rating-1" class="mask mask-star" />
-              <input type="radio" name="rating-1" class="mask mask-star" />
-              <input type="radio" name="rating-1" class="mask mask-star" />
+              <i class="rating-star"></i>
+              <i class="rating-star"></i>
+              <i class="rating-star"></i>
+              <i class="rating-star"></i>
+              <i class="rating-star"></i>
             </div>
           </div>
-          <p class="mb-2">Leave a review on</p>
-          <button
-            class="btn btn-block normal-case bg-[#2F67FA] mb-4 border border-0 mx-auto"
-          >
+          <p class="review-request">Leave a review on</p>
+          <button class="btn btn-block primary" @click="facebookReview">
+            <img src="~/assets/svg/facebook.svg" alt="facebook" />
             facebook
           </button>
-          <button
-            class="btn btn-block outline outline-[#2F67FA] bg-white text-[#2F67FA] normal-case mb-6 border border-0 mx-auto"
-          >
+          <button class="btn btn-block" @click="twitterReview">
+            <img src="~/assets/svg/twitter.svg" alt="twitter" />
             twitter
           </button>
         </div>
@@ -74,4 +65,158 @@ const rateTrade = () => {
     },
   });
 };
+const facebookReview = () => {
+  const appId = "YOUR_FACEBOOK_APP_ID";
+  const pageId = "YOUR_FACEBOOK_PAGE_ID";
+  const reviewLink = `https://www.facebook.com/${pageId}/reviews/?app_id=${appId}`;
+
+  window.open(reviewLink, "_blank");
+};
+
+const twitterReview = () => {
+  const twitterUsername = "YOUR_TWITTER_USERNAME"; // Replace with your Twitter username
+  const reviewText =
+    "I had a great experience with XYZ company! Highly recommended. #XYZCompany"; // Replace with your desired review text
+
+  const tweetLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    reviewText
+  )}&via=${twitterUsername}`;
+
+  window.open(tweetLink, "_blank");
+};
 </script>
+
+<style lang="less" scoped>
+.rating-wrapper {
+  width: 100%;
+  max-width: 345px;
+}
+.greeting--text {
+  font-family: "Lora";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 150%;
+  text-align: center;
+  color: #373d4a;
+  margin-top: 10px;
+}
+.thank-you--text {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  color: #373d4a;
+  margin-top: 10px;
+}
+.rating--text {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+  /* identical to box height, or 27px */
+
+  text-align: center;
+
+  color: #000000;
+}
+.rating--box {
+  margin: 55px 0;
+}
+.rating-star {
+  margin-top: 16px;
+}
+.rating-star {
+  position: relative;
+  cursor: pointer;
+
+  display: inline-block;
+  width: 0;
+  height: 0;
+
+  margin-left: 0.9em;
+  margin-right: 0.9em;
+  margin-bottom: 1.2em;
+
+  border-right: 0.3em solid transparent;
+  border-bottom: 0.7em solid #000;
+  border-left: 0.3em solid transparent;
+
+  /* Controlls the size of the stars. */
+  font-size: 16px;
+
+  &.active {
+    border-bottom: 0.7em solid #fc0;
+  }
+
+  &:before,
+  &:after {
+    content: "";
+
+    display: block;
+    width: 0;
+    height: 0;
+
+    position: absolute;
+    top: 0.6em;
+    left: -1em;
+
+    border-right: 1em solid transparent;
+    border-bottom: 0.7em solid #000;
+    border-left: 1em solid transparent;
+
+    transform: rotate(-35deg);
+  }
+
+  &.active {
+    &:before,
+    &:after {
+      border-bottom: 0.7em solid #fc0;
+    }
+  }
+
+  &:after {
+    transform: rotate(35deg);
+  }
+}
+.review-request {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 150%;
+  margin-bottom: 16px;
+  /* identical to box height, or 27px */
+
+  text-align: center;
+
+  color: #000000;
+}
+.btn {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 150%;
+  text-transform: capitalize;
+  margin-bottom: 8px;
+  background: #ffffff;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  border: none;
+  color: #1d9bf0;
+
+  img {
+    width: 23px;
+    margin-right: 15px;
+  }
+
+  &.primary {
+    color: #ffffff;
+    background: #1877f2;
+    border-color: #1877f2;
+  }
+}
+</style>
