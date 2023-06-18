1b2f96d6c8c6e1c1c353ecdd94ffa06fb6f24df8
Peter Olayinka <peterolayinka97@gmail.com>
2023-06-18 21:15:52 +0100

    Clear data upon submission.

--- pages/identity_success.vue
+++ pages/identity_success.vue
@@ -8,8 +8,8 @@
                     <div class="success">
                         <img src="~/assets/success.svg" alt="" class='gif' />
                         <p class="p1">Thanks for applying!</p>
-                        <p class="p2">You will be notified if you are qualified.</p>
-                        <a class='a'>Awesome thanks</a>
+                        <p class="p2">We will send you and email shortly once your account is verified.</p>
+                        <a class='a' @click="logout">Awesome thanks</a>
                     </div>
                 </div>
             </div>
@@ -19,7 +19,15 @@
 </template>
 
 <script setup>
+import { useAuthStore } from "~~/store/auth";
+
 definePageMeta({
     middleware: ['auth',],
 });
+
+const logout = (event) => {
+  event.preventDefault();
+  useAuthStore().logout();
+  window.location.reload();
+};
 </script>
